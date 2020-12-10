0ac5b24083f507a14c45287adf294227039a70f8
crlorentzen <crlorentzen@gmail.com>
2020-12-10 10:01:23 -0500

    Add files via upload

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,51 +2,34 @@
 local ctrl_alt       = {"ctrl", "alt"}
 local cmd_ctrl_alt 	 = {"cmd", "ctrl", "alt"}
 local ctrl_alt_shift = {"ctrl", "alt", "shift"}
-local ctrl_shift	 = {"ctrl", "shift"}	
+local ctrl_shift	 = {"ctrl", "shift"}
 
 hs.hotkey.bind(cmd_ctrl_alt, "R", function()
   hs.reload()
 end)
 hs.alert.show("Config loaded")
 
+require('caffeine')
 
-hs.hotkey.bind(cmd_ctrl_alt, "L", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x - 10
-  win:setFrame(f)
-end)
-
--- Screens
+-- BEGIN Move window between displays -----
 -- Move to next screen
 hs.hotkey.bind(cmd_ctrl_alt, "N", function()
 	local win = hs.window.focusedWindow()
 	local screen = win:screen()
 	local nextScreen = screen:next()
-	win:moveToScreen(nextScreen)   
-	
-end)  
+	win:moveToScreen(nextScreen)
 
+end)
 -- Move to previous screen
 hs.hotkey.bind(cmd_ctrl_alt, "P", function()
 	local win = hs.window.focusedWindow()
 	local screen = win:screen()
 	local prevScreen = screen:previous()
-	
+
 	win:moveToScreen(prevScreen)
-	
-end) 
 
---hs.hotkey.bind(cmd_ctrl_alt, "T", hs.grid.resizeWindowTaller)
---hs.hotkey.bind(cmd_ctrl_alt, "", hs.grid.resizeWindowShorter)
---hs.hotkey.bind(cmd_ctrl_alt, "Left", hs.grid.resizeWindowThinner)
---hs.hotkey.bind(cmd_ctrl_alt, "Right", hs.grid.resizeWindowWider)
-
-hs.hotkey.bind(cmd_ctrl_alt, 'H', function() hs.window.focusedWindow():focusWindowWest() end)
-hs.hotkey.bind(cmd_ctrl_alt, 'L', function() hs.window.focusedWindow():focusWindowEast() end)
-hs.hotkey.bind(cmd_ctrl_alt, 'K', function() hs.window.focusedWindow():focusWindowNorth() end)
-hs.hotkey.bind(cmd_ctrl_alt, 'J', function() hs.window.focusedWindow():focusWindowSouth() end)
+end)
+-- END Move window between displays -----
 
 hs.hotkey.bind(cmd_ctrl_alt, 'M', function()
 	local win = hs.window.focusedWindow()
@@ -61,7 +44,6 @@ hs.hotkey.bind(cmd_ctrl_alt, 'M', function()
     win:setFrame(f)
 
 end)
-
 hs.hotkey.bind(cmd_ctrl_alt, 'C', function()
 	local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -75,7 +57,6 @@ hs.hotkey.bind(cmd_ctrl_alt, 'C', function()
     win:setFrame(f)
 
 end)
-
 hs.hotkey.bind(cmd_ctrl_alt, "Up", function()
 	local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -88,7 +69,6 @@ hs.hotkey.bind(cmd_ctrl_alt, "Up", function()
     f.h = max.h / 2
     win:setFrame(f)
 end)
-
 hs.hotkey.bind(cmd_ctrl_alt, "Down", function()
 	local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -102,7 +82,6 @@ hs.hotkey.bind(cmd_ctrl_alt, "Down", function()
     win:setFrame(f)
 end)
 
-
 hs.hotkey.bind(cmd_ctrl_alt, "Left", function()
 	local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -112,10 +91,9 @@ hs.hotkey.bind(cmd_ctrl_alt, "Left", function()
     f.x = max.x
     f.y = max.y
     f.w = max.w / 2
-    f.h = max.h 
+    f.h = max.h
     win:setFrame(f)
 end)
-
 hs.hotkey.bind(cmd_ctrl_alt, "Right", function()
 	local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -125,13 +103,13 @@ hs.hotkey.bind(cmd_ctrl_alt, "Right", function()
     f.x = max.x + max.w / 2
     f.y = max.y
     f.w = max.w / 2
-    f.h = max.h 
+    f.h = max.h
     win:setFrame(f)
-
 end)
 
 -- Move
 hs.hotkey.bind(ctrl_alt, "Up", function()
+	-- Half Size Up
 	local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
@@ -139,14 +117,13 @@ hs.hotkey.bind(ctrl_alt, "Up", function()
 
     --f.x = max.x
     f.y = max.y
-    --f.w = max.w / 2
-    f.h = max.h / 2 
+    -- f.w = max.w / 2
+    f.h = max.h / 2
     win:setFrame(f)
 
 end)
-
 hs.hotkey.bind(ctrl_alt, "Down", function()
-
+	-- Half size Down
 	local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
@@ -154,14 +131,14 @@ hs.hotkey.bind(ctrl_alt, "Down", function()
 
     --f.x = max.x
     f.y = max.y + max.h / 2
+    --f.y = max.y + max.h - f.h
     --f.w = max.w / 2
-    f.h = max.h / 2 
+    f.h = max.h / 2
     win:setFrame(f)
 
 end)
-
 hs.hotkey.bind(ctrl_alt, "Left", function()
-
+	-- Half size Left
 	local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
@@ -170,21 +147,22 @@ hs.hotkey.bind(ctrl_alt, "Left", function()
     f.x = max.x
     --f.y = max.y + max.h / 2
     f.w = max.w / 2
-    --f.h = max.h / 2 
+    --f.h = max.h / 2
     win:setFrame(f)
 
 end)
 hs.hotkey.bind(ctrl_alt, "Right", function()
-
+	-- Half size Right
 	local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
     local max = screen:frame()
 
     f.x = max.x + max.w / 2
+    --f.x = max.x + max.w - f.w
     --f.y = max.y + max.h / 2
     f.w = max.w / 2
-    --f.h = max.h / 2 
+    --f.h = max.h / 2
     win:setFrame(f)
 
 end)
